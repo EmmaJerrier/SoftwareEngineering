38dd392f5bb0bec4521d39356dee21b2cd8cc9b2
Emma Jerrier <106136539+EmmaJerrier@users.noreply.github.com>
2025-03-04 18:22:07 +0000

    modified template for correct coloring

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import * as React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <div className="flex flex-col text-center p-8 text-white bg-MISSIONblack">
-      <div>Mission Critical</div>
+    <div className="bg-MISSIONblack text-white text-sm p-4 md:px-16 flex flex-col md:flex-row justify-between items-center">
+      {/* Left Section - Copyright */}
+      <div className="opacity-75">&copy; 2025 MissionCritical Group. All rights reserved.</div>
+      <div className="flex space-x-4 mt-2 md:mt-0">Mission Critical</div>
+      {/* Links */}
+      <div className="flex space-x-4 mt-2 md:mt-0">
+        <a href="/privacy-policy" className="hover:underline">Privacy Policy</a>
+        <a href="/terms-of-service" className="hover:underline">Terms of Service</a>
+        <a href="/contact" className="hover:underline">Contact Us</a>
+      </div>
+
     </div>
   )
 }
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -2,14 +2,29 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
-    <div className="p-4 w-full flex space-x-8 justify-between items-center bg-MISSIONblack text-white md:px-16">
+    <div className="p-2 w-full flex space-x-4 bg-MISSIONblack text-white md:px-16">
       <img
-        className="h-12"
+        className="h-12 justify-start space-x-1"
         src="/images/rocket-logo.png"
         alt="AdvanceTEC"
       />
-      <div className="flex space-x-4">
+      <div className="p-2 w-full justify-start text-white md:px-16">Mission Critical</div>
+      <div className="p-2 w-full flex space-x-4 justify-end text-white md:px-16">
+      <div>
         <Link to={`/`}>Home</Link>
+      </div>
+      <div>
+        <Link to={'/'}>Personal</Link>
+      </div>
+      <div>
+        <Link to={'/'}>Scoreboard</Link>
+      </div>
+      <div>
+        <Link to={'/'}>Blog</Link>
+      </div>
+      <div>
+        <Link to={'/'}>Login/Sign-Up</Link>
+      </div>
       </div>
     </div>
   )
